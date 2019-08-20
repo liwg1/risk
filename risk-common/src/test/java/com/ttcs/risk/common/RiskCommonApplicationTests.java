ae197037d2d60b94b7f506457db1d1061945bed7
李文刚 <liwg2@yusys.com.cn>
2019-08-20 17:51:38 +0800

    工程模块第一次创建

---
+++ risk-common/src/test/java/com/ttcs/risk/common/RiskCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttcs.risk.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RiskCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
