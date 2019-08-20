3764b07e43b8ebae9d09a4f43c31af5a5827dccd
李文刚 <liwg2@yusys.com.cn>
2019-08-20 17:58:16 +0800

    增加frameWork工程

---
+++ risk-framework/src/main/java/com/ttcs/risk/framework/RiskFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.ttcs.risk.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskFrameworkApplication.class, args);
+    }
+
+}
